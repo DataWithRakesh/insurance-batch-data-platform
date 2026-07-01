cdf367d85ada27dd8efafeea589ade2c41ab1da9
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 15:00:10 +0530

    Update 05_create_error_log.sql

--- sql/05_create_error_log.sql
+++ sql/05_create_error_log.sql
@@ -1,1 +1,11 @@
 
+CREATE TABLE error_log (
+    run_id            VARCHAR(100),
+    pipeline_name     VARCHAR(100),
+    notebook_name     VARCHAR(100),
+    source_name       VARCHAR(100),
+    layer             VARCHAR(50),
+    error_message     VARCHAR(MAX),
+    error_timestamp   DATETIME2,
+    batch_date        DATE
+);
