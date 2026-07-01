e736ce6e1368176a705769ec4998488a64d78588
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 14:58:59 +0530

    Update 01_create_metadata.sql

--- sql/01_create_metadata.sql
+++ sql/01_create_metadata.sql
@@ -1,1 +1,12 @@
 
+CREATE TABLE metadata (
+    source_name          VARCHAR(100) PRIMARY KEY,
+    source_path          VARCHAR(500),
+    bronze_table         VARCHAR(100),
+    silver_table         VARCHAR(100),
+    gold_table           VARCHAR(100),
+    file_format          VARCHAR(20),
+    business_key         VARCHAR(100),
+    watermark_column     VARCHAR(100),
+    is_active            CHAR(1)
+);
