57a2a6052209ff9616f47a91b78db31bc82bc838
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 14:59:14 +0530

    Update 02_create_watermark.sql

--- sql/02_create_watermark.sql
+++ sql/02_create_watermark.sql
@@ -1,1 +1,8 @@
 
+CREATE TABLE watermark (
+    source_name          VARCHAR(100) PRIMARY KEY,
+    watermark_column     VARCHAR(100),
+    last_watermark_value DATETIME2,
+    last_batch_date      DATE,
+    updated_ts           DATETIME2
+);
