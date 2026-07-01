a7f83cefc60c579a5b7bb9bcebb3d0b5a3d28ae4
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 15:00:26 +0530

    Update 06_create_schema_history.sql

--- sql/06_create_schema_history.sql
+++ sql/06_create_schema_history.sql
@@ -1,1 +1,9 @@
 
+CREATE TABLE schema_history (
+    source_name      VARCHAR(100),
+    schema_version   INT,
+    schema_json      NVARCHAR(MAX),
+    detected_on      DATETIME2,
+    change_type      VARCHAR(20),
+    status           VARCHAR(20)
+);
