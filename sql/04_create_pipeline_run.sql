0dd959f51b9c74797ede218792dc279525566ca8
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 14:59:50 +0530

    Update 04_create_pipeline_run.sql

--- sql/04_create_pipeline_run.sql
+++ sql/04_create_pipeline_run.sql
@@ -1,1 +1,11 @@
 
+CREATE TABLE pipeline_run (
+    run_id          VARCHAR(100) PRIMARY KEY,
+    pipeline_name   VARCHAR(100),
+    batch_date      DATE,
+    status          VARCHAR(20),
+    start_time      DATETIME2,
+    end_time        DATETIME2,
+    triggered_by    VARCHAR(50),
+    error_message   VARCHAR(MAX)
+);
