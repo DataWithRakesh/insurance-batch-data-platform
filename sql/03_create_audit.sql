f6419688b730c419e5763ed86ccdd766ca72513b
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-01 14:59:34 +0530

    Update 03_create_audit.sql

--- sql/03_create_audit.sql
+++ sql/03_create_audit.sql
@@ -1,1 +1,14 @@
 
+CREATE TABLE audit (
+    run_id              VARCHAR(100),
+    source_name         VARCHAR(100),
+    layer               VARCHAR(50),
+    batch_date          DATE,
+    records_read        INT,
+    records_written     INT,
+    records_rejected    INT,
+    status              VARCHAR(20),
+    start_time          DATETIME2,
+    end_time            DATETIME2,
+    duration_seconds    INT
+);
