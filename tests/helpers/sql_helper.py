6ae50f35956b169f2b5c642f87e5d430392d4252
Rakesh Nayak <mydatainmotion@gmail.com>
2026-07-16 15:17:06 +0530

    Update sql_helper.py

--- tests/helpers/sql_helper.py
+++ tests/helpers/sql_helper.py
@@ -1,1 +1,14 @@
+import pyodbc
+from helpers.config import *
 
+def get_connection():
+    conn = pyodbc.connect(
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+        f"SERVER={SQL_SERVER};"
+        f"DATABASE={SQL_DATABASE};"
+        f"UID={SQL_USERNAME};"
+        f"PWD={SQL_PASSWORD};"
+        f"Encrypt=yes;"
+        f"TrustServerCertificate=no;"
+    )
+    return conn
